66f7b512c4a1166383295bfc2ac58cac615e2269
Kristin <coldzonemt@yahoo.com>
2016-03-19 21:06:16 -0700

    initialize board and begin to initialize pawn class

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # k-chess
+My first chess application. First I will implement the 
+game logic in python, then I will build out a front-end in JavaScript. Finally, I will build a chess engine to play against whoever wants a game! 
+
+1. Python chess game. Board file contains the rules. 
+2. Piece classes contain the legality of their moves
---
+++ gameLib/board.py
@@ -0,0 +1,29 @@
+#import the pieces
+#do the logic of play here
+#make a class for each piece
+'''
+The board class is where I will store the state of the game. 
+There are two properties of the class (so far): the board, and
+the number of turns (for determining if it is black or white's 
+turn, and one of the draw rules). 
+'''
+
+class board(object): 
+	def __init__(self): 
+		self.board = [
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0]
+		]
+		self.turns = 0
+
+def startGame(): 
+	#set up initial board
+
+def updateBoard(move): 
+	#update the board
